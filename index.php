8a813113e6a5fad6ea420e529409eeeaa7332a32
headlampdev <dev@headlamp.us>
2014-06-27 23:37:35 -0700

    Initial project version

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html lang="en"><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="fonts/fonts.css" rel="stylesheet">
+
+
+
+
+	<!-- <link rel="stylesheet" href="css/style.min.css" type="text/css" media="screen"> -->
+	<!--[if IE]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+
+
+
+<body>
+<header>
+</header>
+
+<div class="row zero-padmarg" id="homepage-leader">
+
+		<div class="row zero-padmarg">
+			<div class="col-md-12 nav-paddingLR">
+				<h2 class="logo pull-left">Headlamp</h2>
+				<ul class="nav-header">
+					<li><a href="">What We Do</a></li>
+					<li><a href="">Our Work</a></li>
+					<li><a href="">Our Processes</a></li>
+					<li><a href="">Who We Are</a></li>
+				</ul>
+			</div>
+		</div>
+
+
+
+	<div class="container zero-padding">
+		<div class="row">
+			<div class="col-md-12">
+				<h3 class="leader-copy text-center">We'll Help You Find Your Path to Success</h3>
+			</div>
+
+
+		</div>
+	</div>
+</div>
+
+
+<div class="row zero-padmarg">
+	<div class="col-md-4 text-center border-right service-offering">
+		<h4 class="heading">Product Design</h4>
+		<p>Creating products that solve problems</p>
+	</div>
+
+	<div class="col-md-4 text-center border-right service-offering">
+		<h4 class="heading">User Interface</h4>
+		<p>Designing websites</p>
+	</div>
+
+	<div class="col-md-4 text-center service-offering">
+		<h4 class="heading">User Experience</h4>
+		<p>Research &amp; Creating Engagement</p>
+	</div>
+
+</div>
+
+
+
+<div class="row zero-padmarg">
+
+		<img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="width100" />
+
+</div>
+
+
+
+
+<div class="row zero-padmarg bg-black">
+	<div class="col-process process-offering text-center">
+		<h4 class="heading">Research</h4>
+	</div>
+	<div class="col-process process-offering text-center">
+		<h4 class="heading">Design</h4>
+	</div>
+	<div class="col-process process-offering text-center">
+		<h4 class="heading">Prototype</h4>
+	</div>
+	<div class="col-process process-offering text-center">
+		<h4 class="heading">Build</h4>
+	</div>
+	<div class="col-process process-offering text-center">
+		<h4 class="heading">Ship</h4>
+	</div>
+</div>
+
+
+<footer>
+<div class="row zero-padmarg" id="homepage-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9">
+				here is all the stuff.
+			</div>
+			<div class="col-md3">
+
+			</div>
+
+		</div>
+	</div>
+</div>
+</footer>
+
+
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
