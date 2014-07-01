ab23f65c50e6dd29e8d47c7a32ca0d3b23010001
headlampdev <dev@headlamp.us>
2014-07-01 08:02:35 -0700

    Updated homepage design

--- index.php
+++ index.php
@@ -54,37 +54,104 @@
 	</div>
 </div>
 
+<div class="row zero-padmarg">
+	<div class="span12 text-center">
+		<h3 class="section-title">What We Do</h3>
+		<p class="fifty-padding-bottom">Our services are meant to bring value added results to your business.</p>
+	</div>
+</div>
 
 <div class="row zero-padmarg">
-	<div class="col-md-4 text-center border-right service-offering">
+	<a class="col-md-4 text-center bg-product">
 		<h4 class="heading">Product Design</h4>
 		<p>Creating products that solve problems</p>
-	</div>
-
-	<div class="col-md-4 text-center border-right service-offering">
+	</a>
+	<a class="col-md-4 text-center bg-ux">
 		<h4 class="heading">User Interface</h4>
 		<p>Designing websites</p>
-	</div>
-
-	<div class="col-md-4 text-center service-offering">
-		<h4 class="heading">User Experience</h4>
+	</a>
+	<a class="col-md-4 text-center bg-ui">
+ 		<h4 class="heading">User Experience</h4>
 		<p>Research &amp; Creating Engagement</p>
-	</div>
-
+	</a>
 </div>
 
+<div class="row zero-padmarg bg-black">
+	<div class="col-md-4 text-left">
+		<h3 class="section-title font-white">Work We've Done</h3>
+	</div>
+	<div class="col-md-8 text-right">
+		<p class="forty-padding-top font-white">Our services bring value added results to business.</p>
+	</div>
+</div>
 
 
 <div class="row zero-padmarg">
+	<div class="span12"> 
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		  </ol>
 
-		<img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="width100" />
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner">
+		    <div class="item active">
+		    	<a href="">
+		    	  <img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="img-responsive width100" />
+			      <div class="carousel-caption">
+			        ...
+			      	</div>
+			     </a>
+		    </div>
+		    <div class="item">
+		      <img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="img-responsive width100" />
+		      <div class="carousel-caption">
+		        ...
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="img-responsive width100" />
+		      <div class="carousel-caption">
+		        ...
+		      </div>
+		    </div>
+		  </div>
 
+
+		</div>
+	</div>
+
+
+
+	
 </div>
 
 
+<div class="row zero-padmarg">
+	<div class="col-md-6 text-center">
+		<h3 class="section-title">Our Field Journal</h3>
+		<p class="gateway">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+	<div class="col-md-6 text-center">
+		<h3 class="section-title">Concepts We've Designed</h3>
+		<p class="gateway">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
 
-
-<div class="row zero-padmarg bg-black">
+<!-- <div class="row zero-padmarg bg-black">
 	<div class="col-process process-offering text-center">
 		<h4 class="heading">Research</h4>
 	</div>
@@ -100,7 +167,7 @@
 	<div class="col-process process-offering text-center">
 		<h4 class="heading">Ship</h4>
 	</div>
-</div>
+</div> -->
 
 
 <footer>
