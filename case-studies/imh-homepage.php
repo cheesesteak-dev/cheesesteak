4e05505733a103f1f4c3898f024fcfc13561b1f8
Headlamp Dev <dev@headlamp.us>
2014-07-02 17:27:11 -0700

    Updated case study homepage

--- case-studies/imh-homepage.php
+++ case-studies/imh-homepage.php
@@ -9,157 +9,207 @@ $nav_bg = "bg-black"
 
 <?php include "../_inc/main-nav.php"; ?>
 
-<div class="row zero-padmarg">
-
-
-	<div class="container zero-padding">
-		<div class="row">
-			<div class="col-md-12">
-				<h3 class="leader-copy text-center">Case Study: Corporate Homepage</h3>
-			</div>
+<div class="container-fluid leader">
+	<div class="row zero-padmarg ">
+		<div class="col-md-12">
+			<h3 class="leader-copy text-center">Case Study: Corporate Homepage</h3>
 		</div>
 	</div>
 </div>
-
-
-<div class="row zero-padmarg">
-	<div class="span12 text-center">
-		<h3 class="section-title">What We Do</h3>
-		<p class="fifty-padding-bottom">Our services are meant to bring value added results to your business.</p>
+<div class="container-fluid section-break case-border">
+	<div class="row zero-padmarg">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title">What We Do</h3>
+			<p class="fifty-padding-bottom">Our services are meant to bring value added results to your business.</p>
+		</div>
 	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="col-md-8">
-			<img src="../img/case-studies/imh-homepage/ux-case-id-problem.jpg" />
+	<div class="row zero-padmarg">
+		<div class="col-md-6 col-md-offset-1">
+			<img src="http://placehold.it/823x500" class="polaroid img-responsive" />
 		</div>
 		<div class="col-md-4">
-			asdfase
+			<p>After sitting on the same design since 2007, we finally launched a redesigned website.</p>
+			<p>With it brought an updated style guide, a fresh design, and of course a whole new experience. Also included was about 75% of the original copy throughout the site. What resulted was inconsistent experiences since the copy was not properly matching up with the new user flows.</p>
+			<p>We had decided to tackle this page to improve the user experience through new copy and an overall design update.</p>
+			<p>Tasked as the lead, it would be up to me to improve the user experience. This case study is an overview of the work I did to get to my end goal. In the end, it took about 2 weeks to come up with a finalized design, and another few months to iterate a winning design.</p>
 		</div>
 	</div>
 </div>
 
-<div class="row zero-padmarg bg-black">
-	<div class="col-md-4 text-left">
-		<h3 class="section-title font-white">Work We've Done</h3>
-	</div>
-	<div class="col-md-8 text-right">
-		<p class="forty-padding-top font-white">Our services bring value added results to business.</p>
-	</div>
-</div>
-
-
-<div class="row zero-padmarg">
-	<div class="span12"> 
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-		  </ol>
-
-		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner">
-		    <div class="item active">
-		    	<a href="">
-		    	  <img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="img-responsive width100" />
-			      <div class="carousel-caption">
-			        ...
-			      	</div>
-			     </a>
-		    </div>
-		    <div class="item">
-		      <img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="img-responsive width100" />
-		      <div class="carousel-caption">
-		        ...
-		      </div>
-		    </div>
-		    <div class="item">
-		      <img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="img-responsive width100" />
-		      <div class="carousel-caption">
-		        ...
-		      </div>
-		    </div>
-		  </div>
-
-
+<div class="container-fluid">
+	<div class="row three-up">
+		<div class="col-md-12 text-center">
+			<h2>Setting Goals</h2>
+			<p>During brainstorming sessions, we were able to identify clear goals that we wanted the new design to accomplish.</p>
 		</div>
-	</div>
-
-
-
-	
-</div>
-
-
-<div class="row zero-padmarg">
-	<div class="col-md-6 text-center">
-		<h3 class="section-title">Our Field Journal</h3>
-		<p class="gateway">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
-	<div class="col-md-6 text-center">
-		<h3 class="section-title">Concepts We've Designed</h3>
-		<p class="gateway">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	</div>
-</div>
-
-<!-- <div class="row zero-padmarg bg-black">
-	<div class="col-process process-offering text-center">
-		<h4 class="heading">Research</h4>
-	</div>
-	<div class="col-process process-offering text-center">
-		<h4 class="heading">Design</h4>
-	</div>
-	<div class="col-process process-offering text-center">
-		<h4 class="heading">Prototype</h4>
-	</div>
-	<div class="col-process process-offering text-center">
-		<h4 class="heading">Build</h4>
-	</div>
-	<div class="col-process process-offering text-center">
-		<h4 class="heading">Ship</h4>
-	</div>
-</div> -->
-
-
-<footer>
-<div class="row zero-padmarg" id="homepage-footer">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-9">
-				here is all the stuff.
+		<div class="row zero-padmarg">
+			<div class="col-md-4 text-center">
+				<h4>Improve Conversion</h4>
 			</div>
-			<div class="col-md3">
-
+			<div class="col-md-4 text-center">
+				<h4>Increase Engagement</h4>
 			</div>
-
+			<div class="col-md-4 text-center">
+				<h4>Build Trust</h4>
+			</div>
 		</div>
 	</div>
 </div>
-</footer>
+
+<div class="container-fluid section-break">
+	<div class="row zero-padmarg">
+		<div class="col-md-12 text-center">
+			<h2>Ideating Concepts</h2>
+		</div>
+	</div>
+	<div class="row zero-padmarg">
+	<div class="col-md-7">
+		<img src="../img/case-studies/imh-homepage/ux-case-ideate.jpg" class="polaroid img-responsive" />
+	</div>
+		<div class="col-md-5">
+			<p>Using the whiteboard, I wrote out our target personas, created user stories, drew out user funnels, laid out content flows, mapped exit paths, and put my ideas up front and center.</p>
+			<p>This allowed me to organize my ideas and data so that I can easily guide my team in our feedback meetings. In these meetings, I walked the team through user stories and guided them through various scenarios. This helped validate my ideas.</p>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid section-break case-border">
+	<div class="row zero-padmarg zero-padmarg">
+		<div class="col-md-12 text-center">
+			<h2>Wireframing</h2>
+		</div>
+	</div>
+	<div class="row zero-padmarg">
+		<div class="col-md-4 col-md-offset-1">
+			<p>I transferred my whiteboard designs into a digital version. This wireframing allowed me to align elements in compliance with our grid, incorporate elements from our style guide, and most importantly, give us a more accurate picture of how the homepage will look.</p>
+			<p>This wireframe was kept monochromatic so that during review meetings, the focus would be kept on layout. Conversations about colors and background images would be saved for another date.</p>
+		</div>
+		<div class="col-md-7">
+			<img src="../img/case-studies/imh-homepage/ux-case-wireframing.jpg" class="polaroid img-responsive" />
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid section-break">
+	<div class="row zero-padmarg zero-padmarg">
+		<div class="col-md-12 text-center">
+			<h2>Desiging Concepts</h2>
+			<p>There were divergent viewpoints that we had wanted to implement. Because of this, we had created different concepts to show our senior stakeholders. These two early concepts were produced at the same time.</p>
+		</div>
+	</div>
+	<div class="row zero-padmarg">
+		<div class="col-md-6">
+			<img src="../img/case-studies/imh-homepage/ux-case-concept-design-a.jpg" class="polaroid img-responsive" />
+			<p class="text-justify">This was my original concept. Based on the users we were targeting, I felt this design would be a better fit. It showed exactly what we had to offer and provided a direct path for users to follow. Also in this case, these users would be more likely to scroll than to outright abandon the page.</p>
+			<p>BUTTONS</p>
+		</div>
+		<div class="col-md-6">
+			<img src="../img/case-studies/imh-homepage/ux-case-concept-design-b.jpg" class="polaroid img-responsive" />
+			<p class="text-justify">This design was created to placate some concerns – which was that the original design did not properly convey the brand. Especially of concern was the lack of trust builders and competitive advantages above the fold. This design incorporated all of those elements.</p>
+			<p>BUTTONS</p>
+		</div>
+	</div>
+	<div class="row zero-padmarg">
+		<div class="col-md-12 text-center">
+			<p>After some compromises and iterations, the final design incorporated both aspects of the above comps.</p>
+		</div>
+	</div>
+</div>
 
 
+<div class="container-fluid section-break">
+	<div class="row zero-padmarg">
+		<div class="col-md-12 text-center">
+			<h2>Breaking Down the Final Design</h2>
+		</div>
+	</div>
+	<div class="row zero-padmarg breakdown-break">
+		<div class="col-md-7">
+			<img src="../img/case-studies/imh-homepage/ux-case-goals-1.jpg" class="polaroid img-responsive" />
+		</div>
+		<div class="col-md-5">
+			<h4 class="break">New Sliders</h4>
+			<p>We changed our banners and made them more brand focused. This quote from Gizmodo was meant to validate our brand by creating instant trust by association.</p>
+		</div>
+	</div>
+	<div class="row zero-padmarg breakdown-break">
+		<div class="col-md-7">
+			<img src="../img/case-studies/imh-homepage/ux-case-goals-2.jpg" class="polaroid img-responsive" />
+		</div>
+		<div class="col-md-5">
+			<h4 class="break">Competitive Advantages</h4>
+			<p>These competitive advantages supported the above banner copy. Its placement prominently above the fold gave really focused attention on the brand instead of product.</p>
+		</div>
+	</div>
+	<div class="row zero-padmarg breakdown-break">
+		<div class="col-md-7">
+			<img src="../img/case-studies/imh-homepage/ux-case-goals-3.jpg" class="polaroid img-responsive" />
+		</div>
+		<div class="col-md-5">
+			<h4 class="break">Product Navigation</h4>
+			<p>Each product button was enlarged significantly. We felt that this clean and simple button design would be less distracting to our users.</p>
+		</div>
+	</div>
+	<div class="row zero-padmarg breakdown-break">
+		<div class="col-md-7">
+			<img src="../img/case-studies/imh-homepage/ux-case-goals-4.jpg" class="polaroid img-responsive" />
+		</div>
+		<div class="col-md-5">
+			<h4 class="break">Trust Builders</h4>
+			<p>We wanted to put the human element on our website. So for this section, I interviewed a couple of team members from our support team and asked questions that were more about them, and not focused too much on the company.</p>
+		</div>
+	</div>
+	<div class="row zero-padmarg breakdown-break">
+		<div class="col-md-7">
+			<img src="../img/case-studies/imh-homepage/ux-case-goals-5.jpg" class="polaroid img-responsive" />
+		</div>
+		<div class="col-md-5">
+			<h4 class="break">Friendly Faces</h4>
+			<p>We wanted to put the human element on our website. So for this section, I interviewed a couple of team members from our support team and asked questions that were more about them, and not focused too much on the company.</p>
+		</div>
+	</div>
+	<div class="row zero-padmarg">
+		<div class="col-md-12">
+			[ BUTTON ]
+		</div>
+	</div>
+</div>
 
+<div class="container-fluid section-break">
+	<div class="row zero-padmarg">
+		<div class="col-md-12 text-center">
+			<h2>A/B Testing</h2>
+		</div>
+	</div>
+	<div class="row zero-padmarg">
+		<div class="col-md-4 col-md-offset-1">
+			<p>Finally, to validate the new design against our goals, we pit the new against the original. The initial results varied and for a while underperformed. But the data we were able to gather from these tests proved invaluable. This information allowed us to iterate until we produced a design that outperformed the original.</p>
+		</div>
+		<div class="col-md-6">
+			<img src="http://placehold.it/823x500" class="polaroid img-responsive" />
+		</div>
+	</div>
+</div>
 
+<div class="container-fluid">
+	<div class="row three-up">
+		<div class="col-md-12 text-center">
+			<h2>Setting Goals</h2>
+			<p>During brainstorming sessions, we were able to identify clear goals that we wanted the new design to accomplish.</p>
+		</div>
+		<div class="col-md-4 text-center">
+			<h4>Data Analysis</h4>
+		</div>
+		<div class="col-md-4 text-center">
+			<h4>Workting with Executives</h4>
+		</div>
+		<div class="col-md-4 text-center">
+			<h4>Extensive Copywriting</h4>
+		</div>
+	</div>
+</div>
 
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<?php include '../_inc/footer.php' ?>
 
 </body>
 </html>
