93d1a1f01352401c9513bf5b69ec9bbe9961dc79
headlampdev <dev@headlamp.us>
2014-07-08 01:04:58 -0700

    Created additional pages and modified the homepage design.

--- work/product-design.php
+++ work/product-design.php
@@ -9,76 +9,36 @@ $nav_bg = "bg-black"
 
 <?php include "../_inc/main-nav.php"; ?>
 
-
 <div class="container-fluid leader">
-	<div class="row zero-margin ">
+	<div class="row zero-padmarg ">
 		<div class="col-md-12">
-			<h3 class="leader-copy text-center">The Managed Hosting Landing Page a UX Design Case Study</h3>
-		</div>
-	</div>
-</div>
-<div class="container-fluid section-break case-border">
-	<div class="row zero-margin">
-		<div class="col-md-11 col-md-offset-1">
-			<img src="http://placehold.it/1377x600" class="img-responsive" />
-		</div>
-
-	</div>
-</div>
-
-<div class="container-fluid">
-	<div class="row three-up">
-		<div class="col-md-12 text-center">
-			<h3 class="section-title">Identifying Areas of Improvement</h3>
-			<p>During brainstorming sessions, we were able to identify clear goals that we wanted the new design to accomplish.</p>
-		</div>
-		<div class="row zero-margin">
-			<div class="col-md-4 text-center">
-				<h4>Improve Conversion</h4>
-			</div>
-			<div class="col-md-4 text-center">
-				<h4>Increase Engagement</h4>
-			</div>
-			<div class="col-md-4 text-center">
-				<h4>Build Trust</h4>
-			</div>
+			<h3 class="leader-copy text-left">Product Design</h3>
 		</div>
 	</div>
 </div>
 
-<div class="container-fluid section-break case-border">
-	<div class="row zero-margin">
-		<div class="col-md-10 col-md-offset-1 text-center">
-			<h3 class="section-title">How to Solve These Problems</h3>
-		</div>
-	</div>
-	<div class="row zero-margin">
-		<div class="col-md-4 col-md-offset-1">
-			<h4 class="break">Heading Text</h4>
-			<p>Using existing copy as reference, I was able to take important elements and shorten paragraphs down to their bare essentials.</p>
-			<p>I had also went about creating new sections for trust building which included a case study and an employee profile.</p>
-		</div>
-		<div class="col-md-7">
-			<img src="http://placehold.it/823x500" class=" " />
-		</div>
-	</div>
-	<div class="row zero-margin pad-alternate">
-		<div class="col-md-6 col-md-offset-1">
-			<img src="http://placehold.it/823x500" class=" " />
-		</div>
+<div class="container-fluid" >
+	<div class="row">
 		<div class="col-md-4">
-			<h4 class="break">Change the Layout</h4>
-			<p>We scrapped the entire idea page layout and started over. We broke the page down into must-have elements and then organized them by order of importance.</p>
-		</div>	
-	</div>
-	<div class="row zero-margin pad-alternate">
-		<div class="col-md-4 col-md-offset-1">
-			<h4 class="break">Heading Text</h4>
-			<p>Using existing copy as reference, I was able to take important elements and shorten paragraphs down to their bare essentials.</p>
-			<p>I had also went about creating new sections for trust building which included a case study and an employee profile.</p>
+			<h4 class="left-banner">Data Research</h4>
 		</div>
-		<div class="col-md-7">
-			<img src="http://placehold.it/823x500" class=" " />
+		<div class="col-md-8 bg-product-design-1">
+			<div class="overlay">
+				<p>Here's some text</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid" >
+	<div class="row">
+		<div class="col-md-4">
+			<h4 class="left-banner">Data Research</h4>
+		</div>
+		<div class="col-md-8 bg-product-design-1">
+			<div class="overlay">
+				<p>Here's some text</p>
+				<p>Hello</p>
+			</div>
 		</div>
 	</div>
 </div>
@@ -89,7 +49,7 @@ $nav_bg = "bg-black"
 			<h3 class="section-title">Setting Goals</h3>
 			<p>During brainstorming sessions, we were able to identify clear goals that we wanted the new design to accomplish.</p>
 		</div>
-		<div class="row zero-margin">
+		<div class="row zero-padmarg">
 			<div class="col-md-4 text-center">
 				<h4>Improve Conversion</h4>
 			</div>
@@ -103,14 +63,48 @@ $nav_bg = "bg-black"
 	</div>
 </div>
 
+<div class="container-fluid section-break">
+	<div class="row zero-padmarg">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title">Ideating Concepts</h3>
+		</div>
+	</div>
+	<div class="row zero-padmarg">
+	<div class="col-md-6 col-md-offset-1">
+		<img src="http://placehold.it/823x500" class=" img-responsive" />
+	</div>
+		<div class="col-md-4">
+			<p>Using the whiteboard, I wrote out our target personas, created user stories, drew out user funnels, laid out content flows, mapped exit paths, and put my ideas up front and center.</p>
+			<p>This allowed me to organize my ideas and data so that I can easily guide my team in our feedback meetings. In these meetings, I walked the team through user stories and guided them through various scenarios. This helped validate my ideas.</p>
+		</div>
+	</div>
+</div>
+
 <div class="container-fluid section-break case-border">
-	<div class="row zero-margin zero-margin">
+	<div class="row zero-padmarg zero-padmarg">
+		<div class="col-md-12 text-center">
+			<h2>Wireframing</h2>
+		</div>
+	</div>
+	<div class="row zero-padmarg">
+		<div class="col-md-4 col-md-offset-1">
+			<p>I transferred my whiteboard designs into a digital version. This wireframing allowed me to align elements in compliance with our grid, incorporate elements from our style guide, and most importantly, give us a more accurate picture of how the homepage will look.</p>
+			<p>This wireframe was kept monochromatic so that during review meetings, the focus would be kept on layout. Conversations about colors and background images would be saved for another date.</p>
+		</div>
+		<div class="col-md-6">
+			<img src="http://placehold.it/823x500" class=" img-responsive" />
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid section-break case-border">
+	<div class="row zero-padmarg zero-padmarg">
 		<div class="col-md-8 col-md-offset-2 text-center">
-			<h3 class="section-title">Iterating Designs to Acheive Goals</h3>
+			<h3 class="section-title">Designing Concepts</h3>
 			<p>There were divergent viewpoints that we had wanted to implement. Because of this, we had created different concepts to show our senior stakeholders. These two early concepts were produced at the same time.</p>
 		</div>
 	</div>
-	<div class="row zero-margin">
+	<div class="row zero-padmarg">
 		<div class="col-md-5 col-md-offset-1">
 			<img src="http://placehold.it/668x800" class="" />
 			<p class="text-justify">This was my original concept. Based on the users we were targeting, I felt this design would be a better fit. It showed exactly what we had to offer and provided a direct path for users to follow. Also in this case, these users would be more likely to scroll than to outright abandon the page.</p>
@@ -122,67 +116,84 @@ $nav_bg = "bg-black"
 			<p>BUTTONS</p>
 		</div>
 	</div>
-	<div class="row zero-margin">
+	<div class="row zero-padmarg">
 		<div class="col-md-12 text-center">
 			<p>After some compromises and iterations, the final design incorporated both aspects of the above comps.</p>
 		</div>
 	</div>
 </div>
 
+
 <div class="container-fluid section-break case-border">
-	<div class="row zero-margin">
+	<div class="row zero-padmarg">
 		<div class="col-md-12 text-center">
 			<h3 class="section-title">Breaking Down the Final Design</h3>
 		</div>
 	</div>
-	<div class="row zero-margin breakdown-break">
+	<div class="row zero-padmarg breakdown-break">
 		<div class="col-md-6 col-md-offset-1">
-			<img src="http://placehold.it/838x300" class="img-responsive" />
+			<img src="http://placehold.it/838x300" class=" img-responsive" />
 		</div>
 		<div class="col-md-4">
 			<h4 class="break">New Sliders</h4>
 			<p>We changed our banners and made them more brand focused. This quote from Gizmodo was meant to validate our brand by creating instant trust by association.</p>
 		</div>
 	</div>
-	<div class="row zero-margin breakdown-break">
+	<div class="row zero-padmarg breakdown-break">
 		<div class="col-md-6 col-md-offset-1">
-			<img src="http://placehold.it/838x300" class="img-responsive" />
+			<img src="http://placehold.it/838x300" class=" img-responsive" />
 		</div>
 		<div class="col-md-4">
 			<h4 class="break">Competitive Advantages</h4>
 			<p>These competitive advantages supported the above banner copy. Its placement prominently above the fold gave really focused attention on the brand instead of product.</p>
 		</div>
 	</div>
-	<div class="row zero-margin breakdown-break">
+	<div class="row zero-padmarg breakdown-break">
 		<div class="col-md-6 col-md-offset-1">
-			<img src="http://placehold.it/838x300" class="img-responsive" />
+			<img src="http://placehold.it/838x300" class=" img-responsive" />
 		</div>
 		<div class="col-md-4">
 			<h4 class="break">Product Navigation</h4>
 			<p>Each product button was enlarged significantly. We felt that this clean and simple button design would be less distracting to our users.</p>
 		</div>
 	</div>
-	<div class="row zero-margin breakdown-break">
+	<div class="row zero-padmarg breakdown-break">
 		<div class="col-md-6 col-md-offset-1">
-			<img src="http://placehold.it/838x300" class="img-responsive" />
+			<img src="http://placehold.it/838x300" class=" img-responsive" />
 		</div>
 		<div class="col-md-4">
 			<h4 class="break">Trust Builders</h4>
 			<p>We wanted to put the human element on our website. So for this section, I interviewed a couple of team members from our support team and asked questions that were more about them, and not focused too much on the company.</p>
 		</div>
 	</div>
-	<div class="row zero-margin breakdown-break">
+	<div class="row zero-padmarg breakdown-break">
 		<div class="col-md-6 col-md-offset-1">
-			<img src="http://placehold.it/838x300" class="img-responsive" />
+			<img src="http://placehold.it/838x300" class=" img-responsive" />
 		</div>
 		<div class="col-md-4">
 			<h4 class="break">Friendly Faces</h4>
 			<p>We wanted to put the human element on our website. So for this section, I interviewed a couple of team members from our support team and asked questions that were more about them, and not focused too much on the company.</p>
 		</div>
 	</div>
-	<div class="row zero-margin">
+	<div class="row zero-padmarg">
 		<div class="col-md-12">
 			[ BUTTON ]
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid section-break case-border">
+	<div class="row zero-padmarg">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title">A/B Testing</h3>
+		</div>
+	</div>
+	<div class="row zero-padmarg">
+		<div class="col-md-4 col-md-offset-1">
+			<p>Finally, to validate the new design against our goals, we pit the new against the original. The initial results varied and for a while underperformed. But the data we were able to gather from these tests proved invaluable. This information allowed us to iterate until we produced a design that outperformed the original.</p>
+		</div>
+		<div class="col-md-6">
+			<img src="http://placehold.it/823x500" class=" img-responsive" />
 		</div>
 	</div>
 </div>
@@ -190,7 +201,7 @@ $nav_bg = "bg-black"
 <div class="container-fluid">
 	<div class="row three-up">
 		<div class="col-md-12 text-center">
-			<h3 class="section-title">What We Accomplished</h3>
+			<h3 class="section-title">Setting Goals</h3>
 			<p>During brainstorming sessions, we were able to identify clear goals that we wanted the new design to accomplish.</p>
 		</div>
 		<div class="col-md-4 text-center">
