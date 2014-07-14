5cbecc59fe61a06831766d032b6e3a2428e5f671
headlampdev <dev@headlamp.us>
2014-07-13 20:06:26 -0700

    Updated imh-homepage-new case study and created new sprint page.

---
+++ case-studies/imh-homepage-new.php
@@ -0,0 +1,274 @@
+<?php 
+$sub_dir = "yes";
+// $nav_bg = "bg-black"
+?>
+
+<?php include '../_inc/head.php' ?>
+
+<body>
+
+
+<div class="container-fluid leader-bg leader-case-study-imh-homepage">
+<?php include "../_inc/main-nav.php"; ?>
+			<h2 class="leaderboard text-center">InMotion Hosting Homepage Case Study</h2>
+	<div class="row">
+		<div class="col-md-12 text-center">
+		</div>
+	</div>
+</div>
+
+ 
+<div class="container-fluid section-break">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 text-center">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</p>
+			<a href="" class="button">See The Design</a>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid bg-f4">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title">Goals</h3>
+		</div>
+	</div>
+	<div class="row text-center hundred1-padding-bottom">
+		<div class="col-md-4">
+			<img src="http://placehold.it/100x100" class="img-responsive center-block icon" />
+			<h4>Left Column</h4>
+			<p>Some Short Text</p>
+		</div>
+		<div class="col-md-4">
+			<img src="http://placehold.it/100x100" class="img-responsive center-block icon" />
+			<h4>Middle Column</h4>
+			<p>Some short text</p>
+		</div>
+		<div class="col-md-4">
+			<img src="http://placehold.it/100x100" class="img-responsive center-block icon" />
+			<h4>Right Column</h4>
+			<p>Some short text</p>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title">How We Get There</h3>
+		</div>
+	</div>
+	<div class="row">
+		<a href="">
+			<div class="col-md-4 text-center zero-padmarg bg-product">
+				<h4 class="heading">Product Design</h4>
+				<p>Creating products that solve problems</p>
+			</div>
+		</a>
+		<a href="">
+			<div class="col-md-4 text-center zero-padmarg bg-ux">
+				<h4 class="heading">User Interface</h4>
+				<p>Designing websites</p>		
+			</div>
+		</a>
+		<a href="">
+			<div class="col-md-4 text-center zero-padmarg bg-ui">
+		 		<h4 class="heading">User Experience</h4>
+				<p>Research &amp; Creating Engagement</p>
+			</div>
+		</a>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title">Ideating Concepts</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-5 description-spacing">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<a href="" class="button">Learn More</a>
+		</div>
+		<div class="col-md-7 zero-padmarg">
+			<img src="http://placehold.it/1000x700" class="img-responsive" />
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title">Wireframing</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-7 zero-padmarg">
+				<img src="http://placehold.it/1000x700" class="img-responsive" />
+		</div>
+		<div class="col-md-5 description-spacing">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row text-center">
+		<div class="col-md-8 col-md-offset-2">
+			<h3 class="section-title">Design Iterations</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4 zero-padmarg">
+			<img src="http://placehold.it/580x300" class="img-responsive" />
+		</div>
+		<div class="col-md-4 zero-padmarg">
+			<img src="http://placehold.it/580x300" class="img-responsive" />
+		</div>
+		<div class="col-md-4 zero-padmarg">
+			<img src="http://placehold.it/580x300" class="img-responsive" />
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4 zero-padmarg">
+			<img src="http://placehold.it/580x300" class="img-responsive" />
+		</div>
+		<div class="col-md-4 zero-padmarg">
+			<img src="http://placehold.it/580x300" class="img-responsive" />
+		</div>
+		<div class="col-md-4 zero-padmarg">
+			<img src="http://placehold.it/580x300" class="img-responsive" />
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title-text">Two Designs</h3>
+			<p>The design challenge was to overall the authority figure when it was set. This is the story of what happened and where it happened.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 zero-padmarg">
+			<img src="http://placehold.it/860x900" class="img-responsive" />
+		</div>
+		<div class="col-md-6 zero-padmarg">
+			<img src="http://placehold.it/860x900" class="img-responsive" />
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid bg-f4">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title">Goals Acheived</h3>
+		</div>
+	</div>
+	<div class="row text-center seventy-padding-bottom">
+		<div class="col-md-4">
+			<img src="http://placehold.it/100x100" class="img-responsive center-block icon" />
+			<h4>Left Column</h4>
+			<p>Some Short Text</p>
+		</div>
+		<div class="col-md-4">
+			<img src="http://placehold.it/100x100" class="img-responsive center-block icon" />
+			<h4>Middle Column</h4>
+			<p>Some short text</p>
+		</div>
+		<div class="col-md-4">
+			<img src="http://placehold.it/100x100" class="img-responsive center-block icon" />
+			<h4>Right Column</h4>
+			<p>Some short text</p>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h3 class="section-title">Final Design Breakdown</h3>
+		</div>
+	</div>
+	<div class="row breakdown-spacing breakdown-spacing">
+		<div class="col-md-6 zero-padmarg">
+			<img src="http://placehold.it/1000x200" class="img-responsive" />
+		</div>
+		<div class="col-md-6 description-spacing">
+			<h4 class="breakdown">Top Section Above</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.</p>
+		</div>
+	</div>
+	<div class="row breakdown-spacing">
+		<div class="col-md-6 zero-padmarg">
+			<img src="http://placehold.it/1000x200" class="img-responsive" />
+		</div>
+		<div class="col-md-6 description-spacing">
+			<h4 class="breakdown">Top Section Above</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.</p>
+		</div>
+	</div>
+	<div class="row breakdown-spacing">
+		<div class="col-md-6 zero-padmarg">
+			<img src="http://placehold.it/1000x200" class="img-responsive" />
+		</div>
+		<div class="col-md-6 description-spacing">
+			<h4 class="breakdown">Top Section Above</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.</p>
+		</div>
+	</div>
+	<div class="row breakdown-spacing">
+		<div class="col-md-6 zero-padmarg">
+			<img src="http://placehold.it/1000x200" class="img-responsive" />
+		</div>
+		<div class="col-md-6 description-spacing">
+			<h4 class="breakdown">Top Section Above</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.</p>
+		</div>
+	</div>
+	<div class="row breakdown-spacing">
+		<div class="col-md-6 zero-padmarg">
+			<img src="http://placehold.it/1000x200" class="img-responsive" />
+		</div>
+		<div class="col-md-6 description-spacing">
+			<h4 class="breakdown">Top Section Above</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.</p>
+		</div>
+	</div>
+</div>
+
+<?php include '../_inc/footer.php' ?>
+
+</body>
+</html>
---
+++ design-sprints.php
@@ -0,0 +1,204 @@
+<?php 
+$sub_dir = "no";
+// $nav_bg = "bg-black"
+?>
+
+<?php include '_inc/head.php' ?>
+
+<body>
+
+
+<div class="container-fluid leader-bg leader-case-study-imh-homepage">
+<?php include "_inc/main-nav.php"; ?>
+			<h2 class="leaderboard text-center">My Design Sprints</h2>
+	<div class="row">
+		<div class="col-md-12 text-center">
+		</div>
+	</div>
+</div>
+
+ 
+<div class="container-fluid section-break">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 text-center">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</p>
+			<img src="http://placehold.it/800x800" class="img-responsive center-block forty-padding-top" />
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid bg-f4">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 text-center">
+			<h3 class="section-title-text">Producing Results</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in.</p>
+		</div>
+	</div>
+	<div class="row text-center forty-padding-top">
+		<div class="col-md-4 zero-padmarg">
+			<img src="http://placehold.it/580x300" class="img-responsive" />
+		</div>
+		<div class="col-md-4 zero-padmarg">
+			<img src="http://placehold.it/580x300/271AAA" class="img-responsive" />
+		</div>
+		<div class="col-md-4 zero-padmarg">
+			<img src="http://placehold.it/580x300" class="img-responsive" />
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row bg-black">
+		<div class="col-md-4 description-spacing">
+			<h3 class="section-title-text-left font-white">Processes</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-3 zero-padmarg">
+			<div class="process-section bg-step-one">
+				<div class="process-copy">
+					<img src="http://placehold.it/50x50" class="img-responsive center-block" />
+					<p class="heading">Understanding</p>
+					<p class="sub-heading">Getting Shit Done</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9 description-spacing">
+			<div class="process-section">
+				<div class="process-copy">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row case-border-design">
+		<div class="col-md-3 zero-padmarg">
+			<div class="process-section bg-step-two">
+				<div class="process-copy">
+					<img src="http://placehold.it/50x50" class="img-responsive center-block" />
+					<p class="heading">Ideating</p>
+					<p class="sub-heading">Getting Shit Done</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9 description-spacing">
+			<div class="process-section">
+				<div class="process-copy">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row case-border-design">
+		<div class="col-md-3 zero-padmarg">
+			<div class="process-section bg-step-three">
+				<div class="process-copy">
+					<img src="http://placehold.it/50x50" class="img-responsive center-block" />
+					<p class="heading">Wireframing</p>
+					<p class="sub-heading">Getting Shit Done</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9 description-spacing">
+			<div class="process-section">
+				<div class="process-copy">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row case-border-design">
+		<div class="col-md-3 zero-padmarg">
+			<div class="process-section bg-step-four">
+				<div class="process-copy">
+					<img src="http://placehold.it/50x50" class="img-responsive center-block" />
+					<p class="heading">Building</p>
+					<p class="sub-heading">Getting Shit Done</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9 description-spacing">
+			<div class="process-section">
+				<div class="process-copy">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row case-border-design">
+		<div class="col-md-3 zero-padmarg">
+			<div class="process-section bg-step-five">
+				<div class="process-copy">
+					<img src="http://placehold.it/50x50" class="img-responsive center-block" />
+					<p class="heading">Testing</p>
+					<p class="sub-heading">Getting Shit Done</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9 description-spacing">
+			<div class="process-section">
+				<div class="process-copy">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row case-border-design">
+		<div class="col-md-8 col-md-offset-2 text-center">
+			<h3 class="section-title-text">Design Sprint Resources</h3>
+			<p>This is my top resouce collection of where I find my information on how design sprints are run.</p>
+		</div>
+	</div>
+	<div class="row forty-padding-top">
+		<div class="col-md-3 zero-padmarg">
+			<img src="http://placehold.it/450x150" class="img-responsive" />
+		</div>
+		<div class="col-md-3 zero-padmarg">
+			<img src="http://placehold.it/450x150" class="img-responsive" />
+		</div>
+		<div class="col-md-3 zero-padmarg">
+			<img src="http://placehold.it/450x150" class="img-responsive" />
+		</div>
+		<div class="col-md-3 zero-padmarg">
+			<img src="http://placehold.it/450x150" class="img-responsive" />
+		</div>
+	</div>
+</div>
+
+<?php include '_inc/footer.php' ?>
+
+</body>
+</html>
