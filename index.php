0f4cfdc60af28f6e8d9632cd2cbca0401bb77aa1
Headlamp Dev <dev@headlamp.us>
2014-07-25 15:59:40 -0700

    Update homepage.

--- index.php
+++ index.php
@@ -12,19 +12,20 @@ $active = "no";
 	<?php include "_inc/main-nav.php"; ?>
 </header>
 
-<div class="container-fluid leader-bg leader-bg-homepage">
-			<h2 class="leaderboard text-center font-white">We'll Help You Find Your Path to Success</h2>
+<div class="container-fluid leader-bg leader-case-study-imh-homepage case-border-bottom">
+			<h2 class="leaderboard text-center">We'll Help You Find Your Path to Success</h2>
 	<div class="row">
 		<div class="col-md-12 text-center">
+		</div>
+		<div id="button">
+		&or;
 		</div>
 	</div>
 </div>
 
-
-<div class="container-fluid">
-	<div class="row thirty-padding-bottom">
+<div class="container-fluid" id="myDiv">
+	<div class="row zero-padmarg fifty-padding-bottom">
 		<div class="col-md-8 col-md-offset-2 text-center">
-			<h3 class="section-title-text">What We Do</h3>
 			<p>Headlamp is an online digital company specializing in creating user experiences, interfaces, and increasing online search presence</p>
 		</div>
 	</div>
@@ -32,94 +33,38 @@ $active = "no";
 		<a href="case-studies/product-design.php">
 			<div class="col-md-4 text-center zero-padmarg bg-product">
 				<p class="heading">Product Design</p>
-				<p>Creating products that solve problems</p>
+				<p>Creating products that solve problems<br /><span class="link">Case Study Coming Soon</span></p>
 			</div>
 		</a>
 		<a href="">
 			<div class="col-md-4 text-center zero-padmarg bg-ux">
 				<p class="heading">User Interface</p>
-				<p>Designing websites</p>		
+				<p>Designing websites<br /><span class="link">Case Study Coming Soon</span></p>		
 			</div>
 		</a>
 		<a href="case-studies/imh-homepage.php">
 			<div class="col-md-4 text-center zero-padmarg bg-ui">
 		 		<p class="heading">User Experience</p>
-				<p>Research &amp; Creating Engagement</p>
+				<p>Research &amp; Creating Engagement<br /><span class="link">View Case Study</span></p>
 			</div>
 		</a>
 	</div>
 </div>
 
-
-<!-- <div class="row zero-padmarg">
-	<div class="col-md-12 text-center">
-		<h3 class="section-title">Work We've Done</h3>
-	</div>
-</div>
-<div class="row zero-padmarg">
-	<div class="span12"> 
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-
-		  <ol class="carousel-indicators">
-		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-		  </ol>
-
-
-		  <div class="carousel-inner">
-		    <div class="item active">
-		    	<a href="">
-		    	  <img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="img-responsive width100" />
-			      <div class="carousel-caption">
-			        ...
-			      	</div>
-			     </a>
-		    </div>
-		    <div class="item">
-		      <img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="img-responsive width100" />
-		      <div class="carousel-caption">
-		        ...
-		      </div>
-		    </div>
-		    <div class="item">
-		      <img src="http://tmacfitness.com/wp-content/uploads/2013/04/Beauty-of-nature-random-4884759-1280-800.jpg" class="img-responsive width100" />
-		      <div class="carousel-caption">
-		        ...
-		      </div>
-		    </div>
-		  </div>
-
-
-		</div>
-	</div>
-</div> -->
-
-
-<div class="row zero-padmarg">
-	<a href="" class="bg-field-journal">
-		<div class="col-md-6 text-center">
-			<h3 class="section-title-text">Our Field Journal</h3>
-			<p class="gateway">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+ <div class="row zero-padmarg">
+	<a href="" class="">
+		<div class="col-md-6 text-center bg-blog">
+			<h3 class="section-title-text">Our Blog</h3>
+			<p class="gateway">Thoughts on design, latest trends, and general information</p>
 		</div>
 	</a>
 	<a href="">
 		<div class="col-md-6 text-center">
-			<h3 class="section-title-text">Concepts We've Designed</h3>
-			<p class="gateway">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<h3 class="section-title-text">Design Concepts</h3>
+			<p class="gateway">Keeping the creative juices flowing through thinking outside the box</p>
 		</div>
 	</a>
-</div>
+</div> 
 
 <?php include '_inc/footer.php' ?>
 
